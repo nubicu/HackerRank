c9a21ac91c35446ff96ce9c71e822ba9da129610
Robert <38554758+nubicu@users.noreply.github.com>
2018-04-20 23:39:42 +0300

    Update BirthdayChocolate.cpp

--- BirthdayChocolate.cpp
+++ BirthdayChocolate.cpp
@@ -1,4 +1,5 @@
 //Birthday Chocolate
+// link: https://www.hackerrank.com/challenges/the-birthday-bar/problem
 /*
 Lily has a chocolate bar that she wants to share it with Ron for his birthday. Each of the squares has an integer on it. 
 She decides to share a contiguous segment of the bar selected such that the length of the segment mathches Ron's birth month and the sum of the 
