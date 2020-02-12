cbce78ebb2b486473dfa64e25a05919b1632f6fa
Robert Filipescu <38554758+nubicu@users.noreply.github.com>
2020-02-13 00:35:49 +0200

    Update SherlockAndSquares.cpp
    Cosmetic change

--- SherlockAndSquares.cpp
+++ SherlockAndSquares.cpp
@@ -1,14 +1,14 @@
 //Sherlock and the Beast
-/* 
-Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value describing a range of integers. 
+/*
+Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value describing a range of integers.
 Sherlock must determine the number of square integers within that range, inclusive of the endpoints.
 Note: A square integer is an integer which is the square of an integer, e.g 1,4,9,16,25 ...
 Input Format
-The first line contains , the number of test cases. 
+The first line contains , the number of test cases.
 Each of the next  lines contains two space-separated integers denoting  and , the starting and ending integers in the ranges.
 Constraints : 1<=t<=100 and 1<=a<=b<=10^9
 Output Format
-For each test case, print the number of square integers in the range on a new line. 
+For each test case, print the number of square integers in the range on a new line.
 */
 
 #include <bits/stdc++.h>
@@ -24,7 +24,7 @@ int squares(int a, int b) {
 int main()
 {
     int t;
-    cout << "number of test cases is : ";
+    cout << "Number of test cases is : ";
     cin >> t;
     for(int a0 = 0; a0 < t; a0++){
         int a;
