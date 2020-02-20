519fca5f01e208bfd81227fca927bcb664eac774
Robert <rbuzabri@visteon.com>
2020-02-20 16:45:27 +0200

    Update description

--- ProblemSolving/Algorithms/Implementation/BreakingRecords.cpp
+++ ProblemSolving/Algorithms/Implementation/BreakingRecords.cpp
@@ -2,53 +2,42 @@
 // Name        : BreakingRecords.cpp
 // Author      : Robert Buzabrici-Filipescu
 // Copyright   : HackerRank
-// Description : Maria plays college basketball and wants to go pro.
-// Each season she maintains a record of her play. She tabulates the number of times
-// she breaks her season record for most points and least points in a game.
-// Points scored in the first game establish her record for the season, and she begins
-// counting from there.
-// For example, assume her scores for the season are represented in the array
-// Scores are in the same order as the games played. She would tabulate her results as
-// follows:
-//                                  Count
-// Game  Score  Minimum  Maximum   Min Max
-// 0      12     12       12       0   0
-// 1      24     12       24       0   1
-// 2      10     10       24       1   1
-// 3      24     10       24       1   1
-// Given Maria's scores for a season, find and print the number of times she breaks
-// her records for most and least points scored during the season.
+// Description : 
+/* Maria plays college basketball and wants to go pro.
+  Each season she maintains a record of her play. She tabulates the number of times
+  she breaks her season record for most points and least points in a game.
+  Points scored in the first game establish her record for the season, and she begins
+  counting from there.
+  For example, assume her scores for the season are represented in the array
+  Scores are in the same order as the games played. She would tabulate her results as
+  follows:
+                                   Count
+Game  Score  Minimum  Maximum   Min Max
+  0      12     12       12       0   0
+  1      24     12       24       0   1
+  2      10     10       24       1   1
+  3      24     10       24       1   1
+  Given Maria's scores for a season, find and print the number of times she breaks
+  her records for most and least points scored during the season.
 
-//Function Description
+Function Description
+  Complete the breakingRecords function in the editor below. It must return an integer
+  array containing the numbers of times she broke her records. Index 0 is for breaking
+  most points records, and index 1 is for breaking least points records.
+  breakingRecords has the following parameter(s):
+      scores: an array of integers
 
-//Complete the breakingRecords function in the editor below. It must return an integer
-// array containing the numbers of times she broke her records. Index 0 is for breaking
-// most points records, and index 1 is for breaking least points records.
+Input Format
+  The first line contains an integer n, the number of games.
+  The second line contains n space-separated integers describing the respective
+  values of score0, score1, ....., score n-1
 
-// breakingRecords has the following parameter(s):
-//    scores: an array of integers
+Constraints : 1 <= n <= 1000; 0 <= scores[i] <= 10^8
 
-// Input Format
-
-// The first line contains an integer n, the number of games.
-// The second line contains n space-separated integers describing the respective
-// values of score0, score1, ....., score n-1
-
-// Constraints
-
-// 1 <= n <= 1000
-// 0 <= scores[i] <= 10^8
-
-// Output Format
-
-// Print two space-seperated integers describing the respective numbers of times her
-// best (highest) score increased and her worst (lowest) score decreased.
-//
-// Sample Input 0
-// 9
-// 10 5 20 20 4 5 2 25 1
-
-
+Output Format
+  Print two space-seperated integers describing the respective numbers of times her
+  best (highest) score increased and her worst (lowest) score decreased.
+*/
 //============================================================================
 
 #include <iostream>
@@ -153,3 +142,8 @@ vector<string> split_string(string input_string) {
 
     return splits;
 }
+
+/* Sample Input 0
+  9
+  10 5 20 20 4 5 2 25 1
+*/
